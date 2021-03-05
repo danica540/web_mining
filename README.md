1288964e8e93238bf75989b0010de49f1dfc2956
danica540 <47634021+danica540@users.noreply.github.com>
2021-03-05 17:11:20 +0100

    Added visualization

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # web_mining
+
+python -m spacy download en_core_web_sm
--- process_tweets.py
+++ process_tweets.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from pprint import pprint
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
@@ -11,6 +12,9 @@ from nltk.chunk import conlltags2tree, tree2conlltags
 import spacy
 from spacy import displacy
 import en_core_web_sm
+import matplotlib.pyplot as plt
+from nltk.probability import FreqDist
+
 nlp = en_core_web_sm.load()
 
 nltk.download('punkt')
@@ -21,6 +25,124 @@ nltk.download('wordnet')
 def _load_csv(filename):
     df = pd.read_csv(filename, na_values=['#NAN'])
     return df
+
+
+def _plot_horizontal_bar_chart(all_words, all_words_count, color, title):
+    fig, ax = plt.subplots()
+    y_pos = np.arange(len(all_words))
+    x = np.array(all_words_count)
+    my_yticks = all_words
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(my_yticks)
+    ax.barh(y_pos, x, align='center', color=color)
+    ax.invert_yaxis()
+    ax.set_title(title)
+    plt.show()
+
+
+def _get_words_and_word_count(all_words_counted):
+    all_words = []
+    all_words_count = []
+    for pair in all_words_counted:
+        all_words.append(pair[0])
+        all_words_count.append(pair[1])
+
+    return all_words, all_words_count
+
+
+def _get_entities_list(tweets):
+    entities_list = []
+    for index, row in tweets.iterrows():
+        sent = row['text']
+        doc = nlp(sent)
+        entities_list.append([(X.text, X.label_) for X in doc.ents])
+    return entities_list
+
+
+def _get_filtered_text_list(tweets):
+    filtered_text_list = []
+    for index, row in tweets.iterrows():
+        stop_words = set(stopwords.words('english'))
+        sent = row['text']
+        sent_letters = re.sub("[^a-zA-Z]", " ", sent).lower()
+        word_tokens = word_tokenize(sent_letters)
+        filtered_sentence = [w for w in word_tokens if not w in stop_words]
+        stemmed_words = []
+        stemmer = WordNetLemmatizer()
+        for word in filtered_sentence:
+            word = stemmer.lemmatize(word)
+            stemmed_words.append(word)
+
+        fdist = FreqDist(stemmed_words)
+        most_frequent_words = fdist.most_common(5)
+        filtered_text_list.append(most_frequent_words)
+    return filtered_text_list
+
+
+def _visualize_tweets(tweets, color):
+    most_common_number = 30
+
+    filtered_text_list = _get_filtered_text_list(tweets)
+    entities_list = _get_entities_list(tweets)
+
+    tweets = tweets.assign(filtered_text=filtered_text_list)
+    tweets = tweets.assign(entities=entities_list)
+
+    pprint(tweets['filtered_text'].head(10))
+    pprint(tweets['entities'].head(10))
+
+    all_entities = []
+    for row in tweets['entities']:
+        if len(row) != 0:
+            for pair in row:
+                if pair[0]:
+                    all_entities.append(pair[0])
+
+    all_words_counted = Counter(all_entities).most_common(most_common_number)
+    all_words, all_words_count = _get_words_and_word_count(all_words_counted)
+    _plot_horizontal_bar_chart(
+        all_words, all_words_count, color, "Most frequent entities")
+
+    all_person_entities = []
+    for row in tweets['entities']:
+        if len(row) != 0:
+            for pair in row:
+                if pair[1]:
+                    if pair[1] == 'PERSON':
+                        all_person_entities.append(pair[0])
+
+    all_words_counted = Counter(
+        all_person_entities).most_common(most_common_number)
+    all_words, all_words_count = _get_words_and_word_count(all_words_counted)
+    _plot_horizontal_bar_chart(
+        all_words, all_words_count, color, "Most frequent person entities")
+
+    non_person_entities = []
+    for row in tweets['entities']:
+        if len(row) != 0:
+            for pair in row:
+                if pair[1]:
+                    if pair[1] != 'PERSON':
+                        non_person_entities.append(pair[0])
+
+    all_words_counted = Counter(
+        non_person_entities).most_common(most_common_number)
+    all_words, all_words_count = _get_words_and_word_count(all_words_counted)
+    _plot_horizontal_bar_chart(
+        all_words, all_words_count, color, "Most frequent non person entities")
+
+    all_filtered_words = []
+    for row in tweets['filtered_text']:
+        if len(row) != 0:
+            for pair in row:
+                if pair[1]:
+                    all_filtered_words.append(pair[0])
+
+    all_words_counted = Counter(
+        all_filtered_words).most_common(most_common_number)
+    all_words, all_words_count = _get_words_and_word_count(all_words_counted)
+    _plot_horizontal_bar_chart(
+        all_words, all_words_count, color, "Most frequent filtered words")
 
 
 if __name__ == "__main__":
@@ -69,59 +191,6 @@ if __name__ == "__main__":
     pprint(donald_tweets['handle'].value_counts())
     pprint(hilary_tweets['handle'].value_counts())
 
-    filtered_text_list = []
-    entities_list = []
+    _visualize_tweets(donald_tweets, "blue")
 
-    for index, row in donald_tweets.iterrows():
-        stop_words = set(stopwords.words('english'))
-        sent = row['text']
-        doc = nlp(sent)
-        sent_letters=re.sub("[^a-zA-Z]", " ", sent).lower()
-        word_tokens = word_tokenize(sent)
-        filtered_sentence = [w for w in word_tokens if not w in stop_words]
-        stemmed_words = []
-        stemmer = WordNetLemmatizer()
-        for word in filtered_sentence:
-            word = stemmer.lemmatize(word)
-            stemmed_words.append(word)
-        entities_list.append([(X.text, X.label_) for X in doc.ents])
-        filtered_text_list.append(stemmed_words)
-
-    donald_tweets = donald_tweets.assign(filtered_text=filtered_text_list)
-    donald_tweets = donald_tweets.assign(entities=entities_list)
-
-    pprint(donald_tweets['filtered_text'].head(10))
-    pprint(donald_tweets['entities'].head(10))
-
-    all_entities=[]
-    for row in donald_tweets['entities']:
-        if len(row) != 0:
-            for pair in row:
-                if pair[0]:
-                    all_entities.append(pair[0])
-    
-    pprint(Counter(all_entities).most_common(10))
-
-    all_person_entities=[]
-    for row in donald_tweets['entities']:
-        if len(row) != 0:
-            for pair in row:
-                if pair[1]:
-                     if pair[1]=='PERSON':
-                        all_person_entities.append(pair[0])
-    
-    pprint(Counter(all_person_entities).most_common(20))
-
-
-    non_person_entities=[]
-    for row in donald_tweets['entities']:
-        if len(row) != 0:
-            for pair in row:
-                if pair[1]:
-                     if pair[1]!='PERSON':
-                        non_person_entities.append(pair[0])
-    
-    pprint(Counter(non_person_entities).most_common(20))
-
-
-
+    _visualize_tweets(hilary_tweets, "deeppink")
